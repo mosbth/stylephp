a755c8eee43aae2f6fb836d0cf0ab49d38557c0b
Mikael Roos <mikael.t.h.roos@gmail.com>
2013-10-28 11:30:58 +0100

    adding configuration file with options and function unit

--- style.php
+++ style.php
@@ -14,7 +14,8 @@
  * 2013-10-28: 
  * Included lessphp 0.4.0.
  * Always send last-modified header.
- * Added style_config.php.
+ * Added style_config.php with configuration parameters.
+ * Added less function for unit()
  *
  * 2012-08-27: 
  * Changed time() to gmtime() to make 304 work.
@@ -26,22 +27,15 @@
  * 2012-04-18: First try.
  *
  */
+//
 // Add config-file if available
-$config = dirname(__FILE__).'/style_config.php';
-if(is_file($config)) {
-  include($config);
+//
+$configFile = dirname(__FILE__).'/style_config.php';
+$config = array('path' => dirname(__FILE__)."/lessphp/lessc.inc.php");
+if(is_file($configFile)) {
+  include($configFile);
 }
-else {
-  define('LESSPHP_PATH', dirname(__FILE__)."/lessphp/lessc.inc.php");
-}
-
-
-// Include the lessphp-compiler
-include(LESSPHP_PATH);
-
-
-// Use gzip if available
-ob_start("ob_gzhandler") or ob_start();
+include($config['path']);
 
 
 /**
@@ -51,8 +45,9 @@ ob_start("ob_gzhandler") or ob_start();
  *
  * @param string $inputFile the filename of the less-file.
  * @param string $outputFile the filename of the css-file to be created.
+ * @param array $config with configuration details.
  */
-function autoCompileLess($inputFile, $outputFile) {
+function autoCompileLess($inputFile, $outputFile, $config) {
   $cacheFile = $inputFile.".cache";
 
   if (file_exists($cacheFile)) {
@@ -62,8 +57,33 @@ function autoCompileLess($inputFile, $outputFile) {
   }
 
   $less = new lessc;
-  $newCache = $less->cachedCompile($cache);
 
+  // Add custom less functions
+  if(isset($config['functions'])) {
+    foreach($config['functions'] as $key => $val) {
+      $less->registerFunction($key, $val);
+    }    
+  }
+
+  // Add import dirs
+  if(isset($config['imports'])) {
+    foreach($config['imports'] as $val) {
+      $less->addImportDir($val);
+    }    
+  }
+
+  // Set output formatter
+  if(isset($config['formatter'])) {
+    $less->setFormatter($config['formatter']);
+  }
+
+  // Preserve comments
+  if(isset($config['comments'])) {
+    $less->setPreserveComments($config['comments']);
+  }
+
+  // Compile a new cache
+  $newCache = $less->cachedCompile($cache);
   if (!is_array($cache) || $newCache["updated"] > $cache["updated"]) {
     file_put_contents($cacheFile, serialize($newCache));
     file_put_contents($outputFile, $newCache['compiled']);
@@ -71,15 +91,22 @@ function autoCompileLess($inputFile, $outputFile) {
 }
 
 
+
+//
 // Compile and output the resulting css-file, use caching whenever suitable.
+//
 $less = 'style.less';
 $css  = 'style.css';
-$changed = autoCompileLess($less, $css);
+$changed = autoCompileLess($less, $css, $config);
 $time = filemtime($css);
 $gmdate = gmdate("D, d M Y H:i:s", $time);
 
 
-// Write it out and leave a response
+
+//
+// Write it out and leave a response, use gzip if available
+//
+ob_start("ob_gzhandler") or ob_start();
 header('Last-Modified: ' . $gmdate . " GMT"); 
 if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $time){  
   header("HTTP/1.0 304 Not Modified");  
@@ -87,6 +114,4 @@ if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODI
   header('Content-type: text/css');
   readfile($css);  
 }
-
-
 
